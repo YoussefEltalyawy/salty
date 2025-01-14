bf9952ec25857b265db87a5f89eafb2f71a0a7dd
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2025-01-14 23:11:05 +0200

    Made Empty Cart Prettier

---
+++ app/components/cart/CartEmpty.tsx
@@ -0,0 +1,39 @@
+import {Link} from '@remix-run/react';
+import {useAside} from '../Aside';
+import type {CartMainProps} from './CartMain';
+import {ShoppingBag} from 'lucide-react';
+
+function CartEmpty({
+  hidden = false,
+}: {
+  hidden: boolean;
+  layout?: CartMainProps['layout'];
+}) {
+  const {close} = useAside();
+  return (
+    <div hidden={hidden}>
+      <div className="flex flex-col items-center justify-center h-96 px-6 text-center">
+        <div className="mb-6 p-6 bg-gray-50 rounded-full">
+          <ShoppingBag className="w-8 h-8 text-brandBeige" />
+        </div>
+
+        <h2 className="text-xl font-medium mb-2">Your cart is empty</h2>
+
+        <p className="text-gray-500 mb-8">
+          Looks like you haven&apos;t added anything yet, let&apos;s get you
+          started!
+        </p>
+
+        <Link
+          to="/collections"
+          onClick={close}
+          prefetch="viewport"
+          className="inline-flex items-center justify-center px-6 py-3 text-sm font-medium text-white bg-black hover:bg-gray-800 transition-colors duration-200 rounded-full"
+        >
+          Continue shopping
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default CartEmpty;
